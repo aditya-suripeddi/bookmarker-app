1596c85e460af2f3e6edf2aaf58be7e7f7355d59
Aditya <suripeddiaditya@gmail.com>
2023-07-09 09:52:42 +0530

    update name of environment variables

--- bookmarker-ui-nextjs/next.config.js
+++ bookmarker-ui-nextjs/next.config.js
@@ -12,11 +12,11 @@ const nextConfig = {
   // swcMinify: true,
 
   serverRuntimeConfig: {
-    API_BASE_URL: process.env.SERVER_SIDE_BASE_URL  // configured in docker-app.yaml
+    API_BASE_URL: process.env.SERVER_SIDE_API_BASE_URL  // configured in docker-app.yaml
   },
 
   publicRuntimeConfig: {
-    API_BASE_URL: process.env.CLIENT_SIDE_BASE_URL // configured in docker-app.yaml
+    API_BASE_URL: process.env.CLIENT_SIDE_API_BASE_URL // configured in docker-app.yaml
   },
 
   async redirects() {
